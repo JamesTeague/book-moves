6dc2f413c30fc1968113a7b265186a55f15ae047
JamesTeague <james@teague.dev>
2022-06-25 16:26:39 -0500

    feat(study): add form and ability to get study from lichess

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,20 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import AiBoard from './components/AiBoard.vue';
+import CreateStudyPage from './pages/create-study-page/CreateStudyPage.vue';
 </script>
 
 <template>
-  <AiBoard />
+  <CreateStudyPage />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/*#app {*/
+/*  font-family: Avenir, Helvetica, Arial, sans-serif;*/
+/*  -webkit-font-smoothing: antialiased;*/
+/*  -moz-osx-font-smoothing: grayscale;*/
+/*  text-align: center;*/
+/*  color: #2c3e50;*/
+/*  margin-top: 60px;*/
+/*}*/
 </style>
