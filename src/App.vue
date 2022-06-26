27face730bf7f78de757b6d99fca4d62d25e842b
JamesTeague <james@teague.dev>
2022-06-26 10:29:06 -0500

    feat(nav): add routing and navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import AiBoard from './components/AiBoard.vue';
-import CreateStudyPage from './pages/create-study-page/CreateStudyPage.vue';
+import Navbar from './components/navbar/Navbar.vue';
 </script>
 
 <template>
-  <CreateStudyPage />
+  <Navbar />
+  <router-view></router-view>
 </template>
 
-<style>
-/*#app {*/
-/*  font-family: Avenir, Helvetica, Arial, sans-serif;*/
-/*  -webkit-font-smoothing: antialiased;*/
-/*  -moz-osx-font-smoothing: grayscale;*/
-/*  text-align: center;*/
-/*  color: #2c3e50;*/
-/*  margin-top: 60px;*/
-/*}*/
-</style>
+<style></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
 import { createApp } from 'vue';
+import CreateStudyPage from './pages/create-study-page/CreateStudyPage.vue';
+import AiBoard from './components/boards/AiBoard/AiBoard.vue';
 import App from './App.vue';
 import './index.css';
 
-createApp(App).mount('#app');
+const routes = [
+  { path: '/', name: 'AiBoard', component: AiBoard },
+  { path: '/study', name: 'CreateStudy', component: CreateStudyPage },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+const app = createApp(App);
+
+app.use(router);
+app.mount('#app');
